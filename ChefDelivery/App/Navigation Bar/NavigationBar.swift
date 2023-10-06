15cc2259a66e4423a4871bf91632fc0b18b636f7
Vitor Henrique <vitor.henrique@xds.com.br>
2023-10-06 10:05:41 -0300

    Creating Navigation Bar

---
+++ ChefDelivery/App/Navigation Bar/NavigationBar.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationBar.swift
+//  ChefDelivery
+//
+//  Created by Vitor Henrique Barreiro Marinho on 06/10/23.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationBar()
+}
